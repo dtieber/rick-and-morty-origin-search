5a390deb8f356f0fd3c84a767e7bffb5d67695ca
Daniel Tieber <daniel.tieber@dynatrace.com>
2022-12-30 09:58:58 +0100

    feat: add custom logging formatter for log level

--- src/logger/logging.formatters.ts
+++ src/logger/logging.formatters.ts
@@ -1,1 +1,3 @@
+export const logLevelFormatter = (label: string, _: number): {level: string | number} => ({ level: label })
+
 export const timestampFormatter = (): string => `,"time":"${new Date(Date.now()).toISOString()}"`
