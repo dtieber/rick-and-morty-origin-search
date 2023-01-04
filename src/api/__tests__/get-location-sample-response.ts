fe09a64f3f41c2eccac80a959120d87b2a42ed97
Daniel Tieber <daniel.tieber@dynatrace.com>
2023-01-04 15:20:06 +0100

    test: add sample response from r&m api for location

---
+++ src/api/__tests__/get-location-sample-response.ts
@@ -0,0 +1,12 @@
+export const sampleLocationResponse = {
+  id: 1,
+  name: 'Earth (C-137)',
+  type: 'Planet',
+  dimension: 'Dimension C-137',
+  residents: [
+    'https://rickandmortyapi.com/api/character/38',
+    'https://rickandmortyapi.com/api/character/45',
+  ],
+  url: 'https://rickandmortyapi.com/api/location/1',
+  created: '2017-11-10T12:42:04.162Z',
+}
