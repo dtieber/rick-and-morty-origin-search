c92ab8c6b5ca310ad871f6e98216707567d62516
Daniel Tieber <daniel.tieber@dynatrace.com>
2023-01-05 09:25:13 +0100

    test: add sample response for gql episodes query

---
+++ src/api/__tests__/query-episodes-sample-response.ts
@@ -0,0 +1,233 @@
+export const sampleQueryEpisodesResponse = {
+  data:{
+    episodes:{
+      info:{
+        count:1,
+      },
+      results:[
+        {
+          name:'Rick Potion #9',
+          episodeRef:'S01E06',
+          characters:[
+            {
+              name:'Rick Sanchez',
+              status:'Alive',
+              species:'Human',
+              origin:{
+                name:'Earth (C-137)',
+                type:'Planet',
+              },
+            },
+            {
+              name:'Morty Smith',
+              status:'Alive',
+              species:'Human',
+              origin:{
+                name:'unknown',
+                type:null,
+              },
+            },
+            {
+              name:'Summer Smith',
+              status:'Alive',
+              species:'Human',
+              origin:{
+                name:'Earth (Replacement Dimension)',
+                type:'Planet',
+              },
+            },
+            {
+              name:'Beth Smith',
+              status:'Alive',
+              species:'Human',
+              origin:{
+                name:'Earth (Replacement Dimension)',
+                type:'Planet',
+              },
+            },
+            {
+              name:'Jerry Smith',
+              status:'Alive',
+              species:'Human',
+              origin:{
+                name:'Earth (Replacement Dimension)',
+                type:'Planet',
+              },
+            },
+            {
+              name:'Beth Smith',
+              status:'Alive',
+              species:'Human',
+              origin:{
+                name:'Earth (C-137)',
+                type:'Planet',
+              },
+            },
+            {
+              name:'Brad',
+              status:'Alive',
+              species:'Human',
+              origin:{
+                name:'Earth (Replacement Dimension)',
+                type:'Planet',
+              },
+            },
+            {
+              name:'Cronenberg Rick',
+              status:'unknown',
+              species:'Cronenberg',
+              origin:{
+                name:'Cronenberg Earth',
+                type:'Planet',
+              },
+            },
+            {
+              name:'Cronenberg Morty',
+              status:'unknown',
+              species:'Cronenberg',
+              origin:{
+                name:'Cronenberg Earth',
+                type:'Planet',
+              },
+            },
+            {
+              name:'Davin',
+              status:'Dead',
+              species:'Human',
+              origin:{
+                name:'Earth (C-137)',
+                type:'Planet',
+              },
+            },
+            {
+              name:'Harold',
+              status:'Alive',
+              species:'Cronenberg',
+              origin:{
+                name:'Earth (C-137)',
+                type:'Planet',
+              },
+            },
+            {
+              name:'Jerry Smith',
+              status:'Alive',
+              species:'Human',
+              origin:{
+                name:'Earth (C-137)',
+                type:'Planet',
+              },
+            },
+            {
+              name:'Jessica',
+              status:'Alive',
+              species:'Cronenberg',
+              origin:{
+                name:'Earth (C-137)',
+                type:'Planet',
+              },
+            },
+            {
+              name:'Jessica\'s Friend',
+              status:'Alive',
+              species:'Human',
+              origin:{
+                name:'Earth (C-137)',
+                type:'Planet',
+              },
+            },
+            {
+              name:'MC Haps',
+              status:'Alive',
+              species:'Human',
+              origin:{
+                name:'Earth (C-137)',
+                type:'Planet',
+              },
+            },
+            {
+              name:'Morty Smith',
+              status:'Dead',
+              species:'Human',
+              origin:{
+                name:'Earth (Replacement Dimension)',
+                type:'Planet',
+              },
+            },
+            {
+              name:'Mr. Goldenfold',
+              status:'Alive',
+              species:'Cronenberg',
+              origin:{
+                name:'Earth (C-137)',
+                type:'Planet',
+              },
+            },
+            {
+              name:'Mrs. Sanchez',
+              status:'unknown',
+              species:'Human',
+              origin:{
+                name:'unknown',
+                type:null,
+              },
+            },
+            {
+              name:'Nancy',
+              status:'Alive',
+              species:'Human',
+              origin:{
+                name:'Earth (Replacement Dimension)',
+                type:'Planet',
+              },
+            },
+            {
+              name:'Principal Vagina',
+              status:'Alive',
+              species:'Cronenberg',
+              origin:{
+                name:'Earth (C-137)',
+                type:'Planet',
+              },
+            },
+            {
+              name:'Rick Sanchez',
+              status:'Dead',
+              species:'Human',
+              origin:{
+                name:'Earth (Replacement Dimension)',
+                type:'Planet',
+              },
+            },
+            {
+              name:'Summer Smith',
+              status:'Alive',
+              species:'Human',
+              origin:{
+                name:'Earth (C-137)',
+                type:'Planet',
+              },
+            },
+            {
+              name:'Tammy Guetermann',
+              status:'Alive',
+              species:'Cronenberg',
+              origin:{
+                name:'Earth (C-137)',
+                type:'Planet',
+              },
+            },
+            {
+              name:'Davin',
+              status:'Dead',
+              species:'Cronenberg',
+              origin:{
+                name:'Earth (C-137)',
+                type:'Planet',
+              },
+            },
+          ],
+        },
+      ],
+    },
+  },
+}
