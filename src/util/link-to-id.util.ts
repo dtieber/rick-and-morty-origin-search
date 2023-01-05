fcefb37a899ac311b64950bfa0a98dbca5aadf64
Daniel Tieber <daniel.tieber@dynatrace.com>
2023-01-05 10:27:56 +0100

    feat: delete util to extract ids from restful resource links

--- src/util/link-to-id.util.ts
+++
@@ -1,6 +0,0 @@
-const rickAndMortyApiLinkPattern = /https:\/\/rickandmortyapi\.com\/api\/[a-z]+\/(\d+)/
-
-export const rickAndMortyResourceLinkToId = (link: string): string | undefined => {
-  const parsed = link.match(rickAndMortyApiLinkPattern)
-  return parsed?.pop()
-}
