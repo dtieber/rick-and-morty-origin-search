6e726f75d3d88401ed53616b579bae91c123b26e
Daniel Tieber <daniel.tieber@dynatrace.com>
2023-01-05 09:26:12 +0100

    feat: add type for episode query response
    although it is fairly safe to rely on the expected outcome, `io-ts` provides us with types and gives us this little extra safety basically for free

---
+++ src/api/__tests__/query-episodes-response.type.test.ts
@@ -0,0 +1,24 @@
+import { describe, expect, it } from '@jest/globals'
+import { isRight, right } from 'fp-ts/Either'
+
+import { QueryEpisodesResponse } from '../query-episodes-response.type'
+import { sampleQueryEpisodesResponse } from './query-episodes-sample-response'
+
+describe('query-episode-response type', () => {
+  it('validates a response', () => {
+    const response = sampleQueryEpisodesResponse
+
+    const result = QueryEpisodesResponse.decode(response.data)
+
+    expect(isRight(result)).toBe(true)
+    expect(result).toEqual(right(sampleQueryEpisodesResponse.data))
+  })
+
+  it('rejects an invalid location', () => {
+    const location = {}
+
+    const parsed = QueryEpisodesResponse.decode(location.toString())
+
+    expect(isRight(parsed)).toBe(false)
+  })
+})
