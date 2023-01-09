6f569398ded6583f852773753f1949762413a58b
Daniel Tieber <daniel.tieber@dynatrace.com>
2023-01-09 10:41:32 +0100

    test: properly shut down server after e2e test

--- src/__tests__/server.test.ts
+++ src/__tests__/server.test.ts
@@ -1,11 +1,21 @@
-import { describe, expect, it } from '@jest/globals'
+import { afterAll, beforeAll, describe, expect, it } from '@jest/globals'
+import type { FastifyInstance } from 'fastify'
 
 import { startServer } from '../server'
 
 describe('server', () => {
-  it('starts up and healthcheck returns 200', async () => {
-    const application = await startServer()
 
+  let application: FastifyInstance
+
+  beforeAll(async() => {
+    application = await startServer()
+  })
+
+  afterAll(async() => {
+    await application.close()
+  })
+
+  it('starts up and healthcheck returns 200', async () => {
     const response = await application.inject({
       method: 'GET',
       url: '/health',
